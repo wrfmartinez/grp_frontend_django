93dc438144e79ed00f06707d6fa582d8c626896d
wrfmartinez <wrmartinez709@gmail.com>
2024-06-07 11:28:18 -0400

    Create Banner component and add styles to the banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+const Banner = () => {
+    return (
+        <header className="hero">
+            <h1 className="hero-title">Shop Products</h1>
+            <h2 className="hero-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. Diam phasellus vestibulum lorem sed.</h2>
+        </header>
+    )
+}
+
+export default Banner
