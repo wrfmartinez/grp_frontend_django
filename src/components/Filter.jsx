cc1d53c4653e816c1c9673394be58a3f8c8e64e2
wrfmartinez <wrmartinez709@gmail.com>
2024-06-07 18:10:44 -0400

    Fix breadcrumb styling issue and routing

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,18 +1,77 @@
 import "./css/Filter.css";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Filter = () => {
-  const [ isActive, setIsActive ] = useState("");
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname.includes(path);
+  };
 
   return (
     <section id="filter">
       <ul className="categories">
-        <li className={({ isActive }) => (isActive ? 'category active' : 'category inactive')}><Link to="shop-products/wood">Wood</Link></li>
-        <li className={({ isActive }) => (isActive ? 'category active' : 'category inactive')}><Link to="shop-products/lighting">Lighting</Link></li>
-        <li className={({ isActive }) => (isActive ? 'category active' : 'category inactive')}><Link to="shop-products/appliances">Appliances</Link></li>
-        <li className={({ isActive }) => (isActive ? 'category active' : 'category inactive')}><Link to="shop-products/landscaping">Landscaping</Link></li>
-        <li className={({ isActive }) => (isActive ? 'category active' : 'category inactive')}><Link to="shop-products/paint">Paint</Link></li>
+        <li className="category">
+          <Link
+            className={`${
+              isActive("shop-products/all") ? "active" : "inactive"
+            }`}
+            to="shop-products/all"
+          >
+            All
+          </Link>
+        </li>
+        <li className="category">
+          <Link
+            className={`${
+              isActive("shop-products/wood") ? "active" : "inactive"
+            }`}
+            to="shop-products/wood"
+          >
+            Wood
+          </Link>
+        </li>
+        <li className="category">
+          <Link
+            className={`${
+              isActive("shop-products/lighting") ? "active" : "inactive"
+            }`}
+            to="shop-products/lighting"
+          >
+            Lighting
+          </Link>
+        </li>
+        <li className="category">
+          <Link
+            className={`${
+              isActive("shop-products/appliances") ? "active" : "inactive"
+            }`}
+            to="shop-products/appliances"
+          >
+            Appliances
+          </Link>
+        </li>
+        <li className="category">
+          <Link
+            className={`${
+              isActive("shop-products/landscaping") ? "active" : "inactive"
+            }`}
+            to="shop-products/landscaping"
+          >
+            Landscaping
+          </Link>
+        </li>
+        <li className="category">
+          <Link
+            className={`${
+              isActive("shop-products/paint") ? "active" : "inactive"
+            }`}
+            to="shop-products/paint"
+          >
+            Paint
+          </Link>
+        </li>
       </ul>
     </section>
   );
